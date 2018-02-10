6cecb58190e897b0f1d967b9c6574c956f98c2dd
parag0910 <paragporwal264@gmail.com>
2018-02-11 02:04:49 +0530

    Delete c2.php

--- c2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection = mysqli_connect('localhost', 'root', 'vid1995','details');
-if (!$connection){
-    die("Database Connection Failed" . mysql_error());
-}
-//$select_db = mysqli_select_db();
-//if (!$select_db){
-//    die("Database Selection Failed" . mysql_error());
-//}
